f83157a8b6b0fedf05a506fa84ca45c574f45f08
hannah68 <reihaneh.naderi1989@gmail.com>
2023-03-08 16:37:46 +0000

    fix style route

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./client/styles/style.css";
+import "./styles/style.css";
 
 ReactDOM.render(
 	<BrowserRouter>
